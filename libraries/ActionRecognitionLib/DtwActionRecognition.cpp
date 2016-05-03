4b6a75095b84eacd3ce63d6905816725e5c51386
jgvictores <jcgvicto@gmail.com>
2016-05-03 16:43:52 +0200

    ok test

--- libraries/ActionRecognitionLib/DtwActionRecognition.cpp
+++ libraries/ActionRecognitionLib/DtwActionRecognition.cpp
@@ -7,6 +7,12 @@ namespace teo
 
 bool DtwActionRecognition::setGeneralized(std::vector<double> &v1)
 {
+    if( v1.size() == 0 )
+    {
+        CD_ERROR("Empty vector!\n");
+        return false;
+    }
+
     return true;
 }
 
