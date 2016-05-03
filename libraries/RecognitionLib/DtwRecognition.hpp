75a5ff3694132ad1635a2d329bd8375dd3b7aafe
jgvictores <jcgvicto@gmail.com>
2016-05-03 16:19:17 +0200

    format for doxy

--- libraries/RecognitionLib/DtwRecognition.hpp
+++ libraries/RecognitionLib/DtwRecognition.hpp
@@ -6,19 +6,19 @@
 namespace teo
 {
 
-//This class receive two trajectories vector defined with vectors and compare them with DTW. It returns a "discrepancy value"
+//! This class receive two trajectories vector defined with vectors and compare them with DTW. It returns a "discrepancy value"
 class DtwRecognition
 {
 
 public:
 
-    /*
+    /**
      * With this fuction we just define the generalized trajectory, which
      * at the end is the one we want to achieve
      */
     bool setGeneralized(std::vector<double> &v1);
 
-    /*
+    /**
      * Compare the vector "v2" with the generalized trajectory
      */
     bool compare(std::vector<double> &v2, double& discrepancy);
