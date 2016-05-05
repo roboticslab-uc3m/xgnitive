04cd7fd5fedf11059b6dceda66988ad449795c20
RaulFdzbis <raulfernandezbis@gmail.com>
2016-05-05 17:50:43 +0200

    improve TestActionRecognitionLib_DTW

--- test/testActionRecognitionLib.cpp
+++ test/testActionRecognitionLib.cpp
@@ -3,6 +3,7 @@
 
 #include "ColorDebug.hpp"
 #include "DtwActionRecognition.hpp"
+#include <iostream>
 
 namespace teo
 {
@@ -37,20 +38,87 @@ protected:
 
 TEST_F( TestActionRecognitionLib, TestActionRecognitionLib_DTW) // -- we call the class that we want to do the test and we assign it a name
 {
-    std::vector<std::vector<double>> generalized;
-    std::vector<std::vector<double>> v2;
+    std::vector<std::vector<double>> generalizedRandom, generalizedForZeroDiscrepancy, generalizedForSimpleDiscrepancy;
+    std::vector<std::vector<double>> attempVectRandom, attempVectforZeroDiscrepancy, attempVectforSimpleDiscrepancy;
     double discrepancy;
+    bool okzero=true;
+    bool oksimple=true;
 
+    //--Random Test-------------------------------------//
     //-- test Generalized
-    generalized.push_back({27,58,48,35,25});
-    generalized.push_back({24,8,3453});
-    bool okg = actionRecognition->setGeneralized( generalized );
+    generalizedRandom.push_back({27,58,48,35,25});
+    generalizedRandom.push_back({24,8,3453,48,89});
+    generalizedRandom.push_back({24,58,22,58,47});
+    bool okg = actionRecognition->setGeneralized( generalizedRandom );
 
     //-- test compare
-    v2.push_back({23,54, 345, 2, 18794});
-    v2.push_back({4,58, 324, 12, 7847});
-    bool okc = actionRecognition->compare(v2,discrepancy);
-    ASSERT_TRUE( okg & okc );
+    attempVectRandom.push_back({23,54, 345, 2, 18794});
+    attempVectRandom.push_back({4,58, 324, 12, 7847});
+    bool okc = actionRecognition->compare(attempVectRandom,discrepancy);
+
+    //--Zero Test----------------------------------------//
+    //-- test Generalized
+    generalizedForZeroDiscrepancy.push_back({27,58,48,35,25});
+    generalizedForZeroDiscrepancy.push_back({24,8,3453,12,43});
+    actionRecognition->setGeneralized( generalizedForZeroDiscrepancy );
+
+    //-- test compare
+    attempVectforZeroDiscrepancy.push_back({27,58,48,35,25});
+    attempVectforZeroDiscrepancy.push_back({24,8,3453,12,43});
+    actionRecognition->compare(attempVectforZeroDiscrepancy,discrepancy);
+
+    if (discrepancy==0.0)
+    {
+        okzero=true;
+    }
+    else
+    {
+        CD_WARNING("ZERO TEST NO PASSED\n");
+        okzero=false;
+    }
+
+
+    //--Simple Test-------------------------------------//
+    //-- test Generalized
+    generalizedForSimpleDiscrepancy.push_back({1});
+    generalizedForSimpleDiscrepancy.push_back({0});
+    generalizedForSimpleDiscrepancy.push_back({1});
+    //double tam=generalizedForSimpleDiscrepancy.size();
+    //CD_INFO("EL TAMAÑO ES: %f",tam);
+    //std::cout<<generalizedForSimpleDiscrepancy[0][0]<<std::endl;
+    actionRecognition->setGeneralized( generalizedForSimpleDiscrepancy );
+
+    //-- test compare
+    attempVectforSimpleDiscrepancy.push_back({2});
+    attempVectforSimpleDiscrepancy.push_back({1});
+    attempVectforSimpleDiscrepancy.push_back({0});
+    actionRecognition->compare(attempVectforSimpleDiscrepancy,discrepancy);
+
+    /*
+     * The Matrix cost using euclidean distance is the following one
+     *
+     *  0    1
+     *  |
+     *  0 -- 1
+     *
+     * The optimal path is the one in the figure, which cost is one.
+     */
+
+
+    if (discrepancy==1)
+    {
+        oksimple=true;
+    }
+    else
+    {
+        CD_WARNING("ZERO TEST NO PASSED\n");
+        oksimple=false;
+    }
+
+    CD_INFO("LA DISCREPANCIA ES %f \n",discrepancy);
+
+
+    ASSERT_TRUE( okg & okc & okzero & oksimple);
 }
 
 }  // namespace teo
