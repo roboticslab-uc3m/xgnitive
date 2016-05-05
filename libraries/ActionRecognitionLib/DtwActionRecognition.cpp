d7bb7e90f89dc71b6ebc5095d8557156932eb162
RaulFdzbis <raulfernandezbis@gmail.com>
2016-05-05 17:50:18 +0200

    cleaning DtwActionRecognition.cpp

--- libraries/ActionRecognitionLib/DtwActionRecognition.cpp
+++ libraries/ActionRecognitionLib/DtwActionRecognition.cpp
@@ -22,21 +22,21 @@ bool DtwActionRecognition::setGeneralized(std::vector<std::vector<double >> &v1)
 bool DtwActionRecognition::compare(std::vector<std::vector<double>> &v2, double& discrepancy)
 {
     //"from" and "to" are of the type std::vector< std::vector< double > > which is a vector of vectors.
-    Aquila::DtwDataType from, to;
+    //Aquila::DtwDataType from, to;
 
-    if( v2.size() != generalizedT.size())
+    if( v2[0].size() != generalizedT[0].size())
     {
         CD_ERROR("DTW ERROR=>Input vectors MUST have the same number of features!\n")
         return false;
     }
 
     //Store input vectors in the two DtwDatype variables
-    from=v2;
-    to=generalizedT;
+    //from=v2;
+    //to=generalizedT;
 
     //dtw Aquila dst
     Aquila::Dtw dtw;
-    discrepancy = dtw.getDistance(from, to);
+    discrepancy = dtw.getDistance(v2, generalizedT);
     CD_INFO("DTW distance (neighbors): %f\n", discrepancy)
     //drawDtwPath(dtw, 1, 1);
 
