84bd072ecb78657302c34020227741d705820139
RaulFdzbis <raulfernandezbis@gmail.com>
2016-05-31 16:34:36 +0200

    Some minor norelevant modifications to FindECF

--- cmake/FindECF.cmake
+++ cmake/FindECF.cmake
@@ -1,11 +1,11 @@
 # - Try to find ECF
 # Once done this will define
-#  ECF_FOUND - System has Aquila
-#  ECF_INCLUDE_DIRS - The Aquila include directories
-#  ECF_LIBRARIES - The libraries needed to use Aquila
-#  ECF_DEFINITIONS - Compiler switches required for using Aquila
+#  ECF_FOUND - System has ECF
+#  ECF_INCLUDE_DIRS - The ECF include directories
+#  ECF_LIBRARIES - The libraries needed to use ECF
+#  ECF_DEFINITIONS - Compiler switches required for using ECF
 
-#set(AQUILA_DEFINITIONS ${PC_LIBXML_CFLAGS_OTHER})
+#set(ECF_DEFINITIONS ${PC_LIBXML_CFLAGS_OTHER})
 
 find_path(ECF_INCLUDE_DIR ECF.h
   $ENV{ECF_DIR}
@@ -18,6 +18,5 @@ find_library(ECF_LIBRARY NAMES ecf libecf )
 
 mark_as_advanced(ECF_INCLUDE_DIR ECF_LIBRARY )
 
-#set(AQUILA_DEFINITIONS -std=c++11 )
 set(ECF_INCLUDE_DIRS ${ECF_INCLUDE_DIR} )
 set(ECF_LIBRARIES ${ECF_LIBRARY} )
