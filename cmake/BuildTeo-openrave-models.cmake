a06215c7786363b3adcec461e83c3c36254bfcf3
RaulFdzbis <raulfernandezbis@gmail.com>
2016-11-29 16:10:00 +0100

    Superbuild first commmit

---
+++ cmake/BuildTeo-openrave-models.cmake
@@ -0,0 +1,8 @@
+# Teo-openrave-models
+include(YCMEPHelper)
+
+ycm_ep_helper(Teo-openrave-models TYPE GIT
+			              STYLE GITHUB
+                          REPOSITORY roboticslab-uc3m/teo-openrave-models.git
+                          TAG develop
+                          COMPONENT superbuild)
