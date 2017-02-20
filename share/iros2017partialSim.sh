c2e390c7428038f695f772b1dc9543ac7d473764
jgvictores <jcgvicto@gmail.com>
2017-02-20 12:38:14 +0100

    more info

--- share/iros2017partialSim.sh
+++ share/iros2017partialSim.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+
 YARP_PORT_PREFIX=/0 /usr/bin/python $HOME/repos/openrave-yarp-plugins/examples/openrave-YarpRobotPaintSquares.py &
+
 YARP_PORT_PREFIX=/1 /usr/bin/python $HOME/repos/openrave-yarp-plugins/examples/openrave-YarpRobotPaintSquares.py &
+
+# test 1
+$HOME/repos/xgnitive/build/bin/cgdaExecutionOETOpenrave $HOME/repos/xgnitive/programs/cgdaExecutionOET/conf/evMono_ecf_params.xml 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+# run all
+$HOME/repos/xgnitive/build/bin/runOET.py
 
