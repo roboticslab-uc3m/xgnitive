2005747acc662604795fc6cf718a9d44103cdd86
Raulfdzbis <rauferna@ing.uc3m.es>
2017-02-24 19:46:38 +0100

    Starting simulationÇ

--- programs/multipleExecutionsBatch.sh
+++ programs/multipleExecutionsBatch.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 clear 
 echo "Hi there! We are entering Batch mode: Performing multiple runs.............................................................."
-Number_of_iterations=50 #Set here the number of iterations you want to perform with the cgda system.
+Number_of_iterations=10 #Set here the number of iterations you want to perform with the cgda system.
 i=1
 echo "The number of iterations that will be perfomed will be $Number_of_iterations"
 #sleep 2
 cd ../build/bin
 while [ $i -le $Number_of_iterations ]
 do
-    ./cgdaExecutionIET
+    ./cgdaExecution
     echo "This is the iteration number $i ........................................................................................"
     i=$((i+1))
     wait
