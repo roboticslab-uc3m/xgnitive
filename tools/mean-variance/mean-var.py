8c1309f62b221d438b0498ac5f348619f6aaa054
Raulfdzbis <rauferna@ing.uc3m.es>
2017-09-15 15:49:10 +0200

    Working mean-var tool

--- tools/mean-variance/mean-var.py
+++ tools/mean-variance/mean-var.py
@@ -9,31 +9,36 @@ import numpy as np
 
 ################### TO USE NOW ###########################
 
-features1=[] #column features to use from trajectory1
+features1=[4] #column features to use from trajectory1
 
-features1=[] #column features to use from trajectory1
+#features1=[] #column features to use from trajectory1
 
-t1="Trajectory1.txt" #Trajectory 1 file name
+d1="data/Trajectory-IET-Iron.txt" #Trajectory 1 file name
 
-t2="Trajectory2.txt" #Trajectory 2 file name
+#d2="data/Data2.txt" #Trajectory 2 file name
 
 ##########################################################
 def main():
     
     #Read Trajectory 1
-    trajectory1=np.loadtxt(t1)
-    t1_clean=trajectory1(:,features1)
-
+    data1=np.loadtxt(d1)
+    d1_clean=data1[:,features1]
+    
+	
     #Read Trajectory 2
-    trajectory2= np.loadtxt(t2)
-    t2_clean=trajectory2(:,features2)
+    #trajectory2= np.loadtxt(t2)
+    #t2_clean=trajectory2(:,features2)
 
     #Calculate mean and variance from trajectories, indeed we just need 1
+    meanArray=[]
+    varArray=[]
 	
+    for i in range(d1_clean.shape[1]):
+        meanArray.append(np.mean(d1_clean[:][:,i]))
+        varArray.append(np.var(d1_clean[:][:,i]))
     
-
-
-
+    np.savetxt("results-mean.txt",meanArray,"%f")
+    np.savetxt("results-var.txt",varArray, "%f")
 
 if __name__ == "__main__":
     main()
