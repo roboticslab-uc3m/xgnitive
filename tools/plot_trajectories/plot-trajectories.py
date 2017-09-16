162341de1adb0b5690ae20d86d447b52f3970c49
Raulfdzbis <rauferna@ing.uc3m.es>
2017-09-16 03:49:27 +0200

    Some important changes to plot trajectories

--- tools/plot_trajectories/plot-trajectories.py
+++ tools/plot_trajectories/plot-trajectories.py
@@ -10,6 +10,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
+font = {'family' : 'normal',
+        'size'   : 18}
+
+plt.rc('font', **font)
+
 ##########################################################
 def main():
 
@@ -20,11 +25,11 @@ def main():
     #Read Trajectory 2
 
     #Traj2= np.loadtxt("Trajectory2.txt")
-    #Traj2= np.loadtxt("Trajectory_OET_BESTS.txt")
+    Traj2= np.loadtxt("iron/Trajectory-OET.txt") #First line of OET is to specify the Time interval, not always T.
 
-    Traj3= np.loadtxt("trajectory-IET.txt")
+    Traj3= np.loadtxt("iron/Trajectory-IET.txt")
 
-    #Traj4= np.loadtxt("Trajectory_FTE.txt")
+    Traj4= np.loadtxt("iron/Trajectory-FTE.txt")
 
     ############################### CHOOSE BEST RESULT #########################
     
@@ -34,11 +39,13 @@ def main():
 
     for i in range(4):  # features
         plt.figure()
-        plt.plot(np.linspace(0,1,Traj1.shape[0]),Traj1[:][:,i])
+	plt.xlabel('Force (N)')
+	plt.ylabel('Time Interval')
+        plt.plot(np.linspace(0,9,Traj1.shape[0]),Traj1[:][:,i])
         #plt.plot(Traj2[:][:,0]*1/8,Traj2[:][:,i+1])
-	#plt.plot(Traj2[:][:,0]*1/8,Traj2[:][:,i+1],'ro')
-	plt.plot(np.linspace(0,1,9),Traj3[:][:,i])
-        #plt.plot(np.linspace(0,1,9),Traj4[:][:,i])
+	plt.plot(Traj2[:][:,0],Traj2[:][:,i+1])
+	plt.plot(np.linspace(0,9,9),Traj3[:][:,i])
+        plt.plot(np.linspace(0,9,9),Traj4[:][:,i])
    
 
 
