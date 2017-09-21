fa6ecc949ae6358a742e5e431eb10d44bdd87090
Raulfdzbis <rauferna@ing.uc3m.es>
2017-09-21 13:10:32 +0200

    paint changes to upload cgdaoet to ICRA2018

--- tools/plot_trajectories/plot.py
+++ tools/plot_trajectories/plot.py
@@ -1,22 +1,29 @@
 #!/usr/bin/python
 import matplotlib.pyplot as plt 
 
-font = {'family' : 'normal',
-        'size'   : 18}
+#import matplotlib
+
+#matplotlib.rcParams['ps.useafm'] = True
+#matplotlib.rcParams['pdf.use14corefonts'] = True
+#matplotlib.rcParams['text.usetex'] = True
+
+font = {'family' : 'Helvetica',
+        'size'   : 36}
 
 plt.rc('font', **font)
 
-plt.plotfile('IET-Paint-Percentage.txt', delimiter=' ', cols=(0, 1), 
-                     names=('Time interval', 'Wall Painted (%)'), marker='o', label='IET')
 
-plt.plotfile('OET-Paint-Percentage.txt', delimiter=' ', cols=(0, 1), newfig=False,
-                     names=('Time interval', 'Wall Painted (%)'), marker='^', label='OET')
+plt.plotfile('Generalized.txt', delimiter=' ', cols=(0, 1),
+                     names=('Time interval', 'Wall Painted (%)'), marker='s', label='Generalized', linewidth=7.0)
 
 plt.plotfile('FTE-Paint-Percentage.txt', delimiter=' ', cols=(0, 1), newfig=False, 
-                     names=('Time interval', 'Wall Painted (%)'), marker='x', label='FTE')
+                     names=('Time interval', 'Wall Painted (%)'), marker='x', label='FTE', linewidth=2.0)
 
-plt.plotfile('Generalized.txt', delimiter=' ', cols=(0, 1), newfig=False,
-                     names=('Time interval', 'Wall Painted (%)'), marker='s', label='Generalized')
+plt.plotfile('IET-Paint-Percentage.txt', delimiter=' ', cols=(0, 1), newfig=False,
+                     names=('Time interval', 'Wall Painted (%)'), marker='o', label='IET', linewidth=2.0)
+
+plt.plotfile('OET-Paint-Percentage.txt', delimiter=' ', cols=(0, 1), newfig=False,
+                     names=('Time interval', 'Wall Painted (%)'), marker='^', label='OET', linewidth=2.0)
 
 plt.legend(loc=2)
 
