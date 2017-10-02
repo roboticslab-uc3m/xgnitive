c43ce3b243e3a98f9daf9498fe7cd1a1df5d1ecb
Raulfdzbis <rauferna@ing.uc3m.es>
2017-10-02 20:06:23 +0200

    Working gridworld with debug each step

---
+++ programs/drl/grid_world.py
@@ -0,0 +1,18 @@
+from rllab.algos.trpo import TRPO
+from rllab.baselines.linear_feature_baseline import LinearFeatureBaseline
+#from rllab.envs.grid_world_env import GridWorldEnv
+from envs.grid_world_env import GridWorldEnv
+from rllab.envs.normalized_env import normalize
+from rllab.policies.categorical_mlp_policy import CategoricalMLPPolicy
+
+env = normalize(GridWorldEnv())
+policy = CategoricalMLPPolicy(
+    env_spec=env.spec,
+)
+baseline = LinearFeatureBaseline(env_spec=env.spec)
+algo = TRPO(
+    env=env,
+    policy=policy,
+    baseline=baseline,
+)
+algo.train()
