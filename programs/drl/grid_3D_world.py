7e8a0d5b73fdcda99f6fb81c48d0ccdabbc77b74
Raulfdzbis <rauferna@ing.uc3m.es>
2017-10-05 15:48:45 +0200

    3D grid world new files

---
+++ programs/drl/grid_3D_world.py
@@ -0,0 +1,21 @@
+from rllab.algos.trpo import TRPO
+from rllab.baselines.linear_feature_baseline import LinearFeatureBaseline
+#from rllab.envs.grid_world_env import GridWorldEnv
+from envs.grid_world_2D_env import GridWorld2DEnv
+from rllab.envs.normalized_env import normalize
+from rllab.policies.categorical_mlp_policy import CategoricalMLPPolicy
+
+env = normalize(GridWorld2DEnv())
+policy = CategoricalMLPPolicy(
+    env_spec=env.spec,
+)
+baseline = LinearFeatureBaseline(env_spec=env.spec)
+algo = TRPO(
+    env=env,
+    policy=policy,
+    baseline=baseline,
+    n_itr=5,
+    # Uncomment both lines (this and the plot parameter below) to enable plotting
+    #plot=True,
+)
+algo.train()
