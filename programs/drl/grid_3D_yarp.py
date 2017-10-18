8ccf78089447578b9cf01c024393a3ee0a78d47a
Raulfdzbis <rauferna@ing.uc3m.es>
2017-10-18 17:59:36 +0200

    new files towards yarp-rllab-teo

---
+++ programs/drl/grid_3D_yarp.py
@@ -0,0 +1,35 @@
+from rllab.algos.trpo import TRPO
+from rllab.baselines.linear_feature_baseline import LinearFeatureBaseline
+#from rllab.envs.grid_world_env import GridWorldEnv
+from envs.grid_world_3D_env_yarp import GridWorld3DEnvYarp
+from rllab.envs.normalized_env import normalize
+from rllab.policies.categorical_mlp_policy import CategoricalMLPPolicy
+from rllab.misc.instrument import run_experiment_lite
+
+def run_task(*_):
+    env = normalize(GridWorld3DEnvYarp())
+    policy = CategoricalMLPPolicy(
+        env_spec=env.spec,
+    )
+    baseline = LinearFeatureBaseline(env_spec=env.spec)
+    algo = TRPO(
+        env=env,
+        policy=policy,
+        baseline=baseline,
+        n_itr=200,
+        # Uncomment both lines (this and the plot parameter below) to enable plotting
+        #plot=True,
+    )
+    algo.train()
+
+run_experiment_lite(
+    run_task,
+    # Number of parallel workers for sampling
+    n_parallel=1,
+    # Only keep the snapshot parameters for the last iteration
+    snapshot_mode="last",
+    # Specifies the seed for the experiment. If this is not provided, a random seed
+    # will be used
+    seed=1,
+    # plot=True,
+)
