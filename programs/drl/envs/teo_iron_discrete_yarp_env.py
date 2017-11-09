7b580e0fccff7a82c04ac22bb3d4a6f2b28676c7
Raulfdzbis <rauferna@ing.uc3m.es>
2017-11-09 17:38:10 +0100

    Starting to work with the iron action

---
+++ programs/drl/envs/teo_iron_discrete_yarp_env.py
@@ -0,0 +1,362 @@
+################### GENERAL ##############################
+import numpy as np
+import time
+import copy
+
+################### RLLAB ################################
+from .base import Env
+from rllab.spaces import Discrete
+from rllab.envs.base import Step
+from rllab.core.serializable import Serializable
+from rllab.spaces import Box
+
+################### YARP ##############################
+import yarp
+
+################### TO USE NOW ############################
+
+################### ENVIRONMENT #############################
+
+class TeoIronDiscreteYarpEnv(Env, Serializable):
+    """
+    'S' : starting point
+    'F' or '.': free space
+    'W' or 'x': wall
+    'H' or 'o': hole (terminates episode)
+    'G' : goal
+    """
+
+    ################### INIT ###############################
+
+    def __init__(self):
+
+        self.yarpDelay=0.01
+        self.action_inc=5
+        self.action_penalty=0.1
+        self.num_step=0
+        self.percentage=0
+
+        ################ YARP ###############################################
+        yarp.Network.init()
+
+        if yarp.Network.checkNetwork() != True:  # let's see if there was actually a reachable YARP network
+            print('[error] Please try running yarp server')  # tell the user to start one with 'yarp server' if there isn't any
+            quit()
+
+        mentalOptions= yarp.Property()
+        
+        ################ YARP CONNECT TO MOVE ###############################
+        # This device will have three different options, device, remote local.
+        mentalOptions.put("device", "controlboardwrapper2")  # device
+        mentalOptions.put("subdevice", "YarpOpenraveControlboard") #device
+        mentalOptions.put("env", "/usr/local/share/xgnitive/contexts/models/teo_cgda_iros.env.xml") #Environment
+        mentalOptions.put("name", "/drl/rightArm")  # Teo
+        mentalOptions.put("robotIndex", 0) #Teo
+        mentalOptions.put("manipulatorIndex", 2) #Right_Arm
+
+        ################ YARP CONNECT TO IRON ###############################
+
+        orPlugins = mentalOptions.addGroup("orPlugins")
+
+        orPlugin1 = orPlugins.addGroup("OpenraveYarpPaintSquares")  # Our lovely plugin (◕‿◕✿))
+        orPlugin1.put("module", "OpenraveYarpPaintSquares")
+        orPlugin1.put("commands", "open /drl/openraveYarpPaintSquares/rpc:s")
+
+        # orPlugin2 = orPlugins.addGroup("OpenraveYarpPaintSquares2")  # Our lovely plugin (◕‿◕✿))
+        # orPlugin2.put("module","OpenraveYarpPaintSquares2a")
+        # orPlugin2.put("commands", "open")
+
+        #define Device
+        #yarp.dev.Polydriver(mentalDevice)
+        self.mentalDevice= yarp.PolyDriver()
+        #open device
+        self.mentalDevice.open(mentalOptions)
+        if not self.mentalDevice.isValid() :
+            print("Mental robot device not available.\n")
+            self.mentalDevice.close()
+            yarp.Network.fini()
+            return 1
+
+        print("Mental robot device available.\n")
+
+
+        ################ YARP CONNECT TO FORCE ###############################
+
+        self.forcePort= yarp.BufferedPortBottle()
+
+        self.forcePort.open("/force:i")
+
+        while True: ##do-while
+            yarp.Network.connect("/forceEstimator:o", "/force:i")
+            print(" Wait to connect to forces...")
+            time.sleep(self.yarpDelay)
+            if self.forcePort.getInputCount() != 0:
+                break
+        print ("Force port available. \n")
+
+        ################ YARP CONNECT TO WORLD ###############################
+
+        self.rpcClientWorld = yarp.RpcClient()
+
+        self.rpcClientWorld.open("/world:c")
+
+        while True: ##do-while
+            yarp.Network.connect("/world:c", "/worldRpcResponder/rpc:s")
+            print(" Wait to connect to world...")
+            time.sleep(self.yarpDelay)
+            if self.rpcClientWorld.getOutputCount() != 0:
+                break
+        print ("Force port available. \n")
+
+        ################ YARP CONNECT TO CARTESIAN ###############################
+
+        self.rpcClientCart = yarp.RpcClient()
+
+        self.rpcClientCart.open("/cart:c")
+
+        while True: ##do-while
+            yarp.Network.connect("/cart:c", "/CartesianControl/rpc_transform:s")
+            print(" Wait to connect to Cartesian...")
+            time.sleep(self.yarpDelay)
+            if self.rpcClientCart.getOutputCount() != 0:
+                break
+        print ("Cartesian port available. \n")
+
+
+        ################ YARP OBTAIN LIMITS ###############################
+        self.mentalPositionControl = self.mentalDevice.viewIPositionControl()
+        self.mentalControlLimits = self.mentalDevice.viewIControlLimits()
+
+        self.min0 = yarp.DVector(1)
+        self.max0 = yarp.DVector(1)
+        self.mentalControlLimits.getLimits(0, self.min0, self.max0)
+
+        self.min1 = yarp.DVector(1)
+        self.max1 = yarp.DVector(1)
+        self.mentalControlLimits.getLimits(1, self.min1, self.max1)
+
+        self.min3 = yarp.DVector(1)
+        self.max3 = yarp.DVector(1)
+        self.mentalControlLimits.getLimits(3, self.min3, self.max3)
+
+        #self.lbound=-15
+        #self.ubound=100
+
+        ############### GENERAL ############################################
+
+        #
+        Serializable.quick_init(self, locals())
+
+        self.start_state = [0,0,0]
+        self.state = None
+        self.domain_fig = None
+
+    ################### RESET #############################
+
+    def reset(self):
+        self.state = copy.deepcopy(self.start_state) #rmbr
+
+        #Set initial position:
+        dEncRaw = np.empty(6,float)
+
+        dEncRaw[0] = 0
+        dEncRaw[1] = 0
+        dEncRaw[3] = 0
+
+        self.mentalPositionControl.positionMove(0, dEncRaw[0])
+        self.mentalPositionControl.positionMove(1, dEncRaw[1])
+        self.mentalPositionControl.positionMove(3, dEncRaw[3])
+
+        print(self.state)
+        time.sleep(self.yarpDelay)  # pause
+
+        # Reset paint
+        self.cmd.clear()
+        self.res.clear()
+        self.cmd.addString("reset")
+        print(self.cmd)
+        self.rpcClient.write(self.cmd,self.res)
+
+        #Reset num_steps
+        self.num_step = 0
+
+        #Set percentage of painted wall as zero
+        self.percentage = 0
+
+
+        return self.state
+
+    ################### ACTION DEFINITION #################
+
+    @staticmethod
+    def action_from_direction(d):
+        """
+        Return the action corresponding to the given direction. This is a helper method for debugging and testing
+        purposes.
+        :return: the action index corresponding to the given direction
+        """
+        return dict( #The actions for the system are:
+            inc_0=0,
+            dec_0=1,
+            inc_1=2,
+            dec_1=3,
+            inc_3=4,
+            dec_3=5
+        )[d]
+
+    ################### STEP ##############################
+
+    def step(self, action):
+        """
+        action map:
+        0: left
+        1: down
+        2: right
+        3: up
+        4: climb_up
+        5: climb_down
+        :param action: should be a one-hot vector encoding the action
+        :return:
+        """
+
+        #Get possible states with the actual state.
+        possible_next_states = self.get_possible_next_states(self.state, action)
+
+        #Choose an action at random with the probabilities obtained in get possible states. This means off-policy
+        probs = [x[1] for x in possible_next_states]
+        next_state_idx = np.random.choice(len(probs), p=probs)
+        next_state = possible_next_states[next_state_idx][0]
+
+        #print("next state before move is", next_state)
+
+        #Try to move the robot.
+
+        #print("the next state is ",next_state)
+        print("Me voy a mover a:", next_state[0],next_state[1],next_state[2])
+        dEncRaw = np.empty(6,float)
+
+        dEncRaw[0] = next_state[0]
+        dEncRaw[1] = next_state[1]
+        dEncRaw[3] = next_state[2]
+
+        #The new state is equal to state where i moved.
+        self.state[0] = next_state[0]
+        self.state[1] = next_state[1]
+        self.state[2] = next_state[2]
+
+        #dEncRaw[0] = 45
+        #dEncRaw[1] = 0
+        #dEncRaw[3] = 0
+
+        self.mentalPositionControl.positionMove(0, dEncRaw[0])
+        self.mentalPositionControl.positionMove(1, dEncRaw[1])
+        self.mentalPositionControl.positionMove(3, dEncRaw[3])
+
+        #Just some sleep for debug
+        #print("Moving the robot")
+        time.sleep(self.yarpDelay)  # pause
+
+        ################ GET CARTESIAN POSITION #################################
+
+        self.cmd.clear()
+        self.res.clear()
+        self.cmd.addString("stat")
+
+        self.rpcClientCart.write(self.cmd, self.res)
+
+        print("La posición obtenida con el Cartesian Position es: ", self.res())
+
+        ################ GET FORCE ##############################################
+
+        #self.cmd.clear()
+        #self.res.clear()
+
+        b = self.forcePort.read(false)
+
+        #Waiting to receive force
+        if not b:
+            while(1):
+                print("No force received yet")
+                if b:
+                    break
+
+        print("La fuerza obtenida es: ", b)
+
+
+        '''
+        self.cmd.clear()
+        self.res.clear()
+        self.cmd.addString("get encs")
+        self.rpcClientCart.write(self.cmd, self.res)
+
+        #print("La posición después de moverme es: ", self.res)
+        print("Got after movement : ", self.res.toString())
+
+        print("--------------------------------------------------------------\n")
+        '''
+
+        #print(self.n_col)
+        #print(self.n_row)
+        #print(self.levels)
+        #print("the next z is", next_z)
+        #print("the next x is", next_x)
+        #print("the next y is", next_y)
+
+        ################ YARP REWARD #################################
+
+        # Idea reward: Executed trajectory - target trajectory. The Executed trajectory is init with 0s. It has to be comulative.
+        # self.trajectory (??)
+
+
+
+        if self.percentage==100:
+            done=True
+        else:
+            done=False
+
+        print ("El reward es: ", reward)
+
+        return Step(observation=self.state, reward=reward, done=done)
+
+    ################### NEXT STATES #############################
+
+    def get_possible_next_states(self, state, action):
+
+        """
+        Given the state and action, return a list of possible next states and their probabilities. Only next states
+        with nonzero probabilities will be returned
+        :param action: action
+        :return: a list of pairs (s', p(s'|s,a))
+        """
+
+        # assert self.observation_space.contains(state)
+        # assert self.action_space.contains(action)
+
+
+        ################### GET FUTURE STATES #############################
+
+        #Possible increments produced by the action:
+        increments = np.array([[self.action_inc, 0, 0], [-self.action_inc, 0, 0], [0, self.action_inc, 0], [0, -self.action_inc, 0], [0, 0, self.action_inc], [0, 0, -self.action_inc]])
+
+        next_state = np.clip(
+            state + increments[action],
+            [self.min0[0], self.min1[0], self.min3[0]], # Limits
+            [self.max0[0], self.max1[0], self.max3[0]]  # Limits
+        )
+
+        print("the next state is ", next_state)
+
+        return [(next_state, 1.)]
+
+    ################### ACTION ##################################
+
+    @property
+    def action_space(self):
+        return Discrete(6)
+
+    ################### OBSERVATION #############################
+
+    @property
+    def observation_space(self):
+        return Box(low=-np.inf, high=np.inf, shape=(3,)) #The state is defined as a 3D continous space
+        #return Discrete(self.n_row * self.n_col * self.levels)
---
+++ programs/drl/teo_iron_discrete_yarp.py
@@ -0,0 +1,39 @@
+from rllab.algos.trpo import TRPO
+from rllab.baselines.linear_feature_baseline import LinearFeatureBaseline
+#from rllab.envs.grid_world_env import GridWorldEnv
+from envs.teo_iron_discrete_yarp_env import TeoIronDiscreteYarpEnv
+from rllab.envs.normalized_env import normalize
+from rllab.policies.categorical_mlp_policy import CategoricalMLPPolicy
+from rllab.misc.instrument import run_experiment_lite
+
+def run_task(*_):
+    env = normalize(TeoIronDiscreteYarpEnv())
+    policy = CategoricalMLPPolicy(
+        env_spec=env.spec,
+    )
+    baseline = LinearFeatureBaseline(env_spec=env.spec)
+    algo = TRPO(
+        env=env,
+        policy=policy,
+        baseline=baseline,
+        batch_size=1000, #The more, the more past samples it takes.
+        max_path_length=400, #Lenght of the learned path.
+        discount=0.99, #discount for discounter
+        step_size=0.01, #Step size of the gradient
+        #n_itr=800,
+        # Uncomment both lines (this and the plot parameter below) to enable plotting
+        #plot=True,
+    )
+    algo.train()
+
+run_experiment_lite(
+    run_task,
+    # Number of parallel workers for sampling
+    n_parallel=1,
+    # Only keep the snapshot parameters for the last iteration
+    snapshot_mode="last",
+    # Specifies the seed for the experiment. If this is not provided, a random seed
+    # will be used
+    # seed=1,
+    # plot=True,
+)
