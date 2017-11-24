1dfe4f6b1d36354b63c7b177c9361990f1eea5ad
Raulfdzbis <rauferna@ing.uc3m.es>
2017-11-24 14:09:44 +0100

    Working setup: REMOTE_INSTREW-DYNAMICLI-NOPEN-BIGREW-NOENC

--- programs/drl/envs/teo_paint_discrete_yarp_env.py
+++ programs/drl/envs/teo_paint_discrete_yarp_env.py
@@ -23,7 +23,7 @@ class TeoPaintDiscreteYarpEnv(Env, Serializable):
 
     def __init__(self):
 
-        self.yarpDelay = 0.1
+        self.yarpDelay = 0.01
         self.action_inc = 5
         self.action_penalty = 0.1
         self.num_step = 0
