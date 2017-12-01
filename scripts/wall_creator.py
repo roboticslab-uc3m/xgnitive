03706ef6018cffd644a90a00bbd633e35ae39f44
Raulfdzbis <rauferna@ing.uc3m.es>
2017-12-01 18:24:30 +0100

    more work with wall creator

--- scripts/wall_creator.py
+++ scripts/wall_creator.py
@@ -11,17 +11,30 @@ EXTENTS=E.Extents
 TRANSLATION=E.Translation
 DIFUSSECOLOR=E.diffuseColor
 
+NSquares=4
+
+Ex=0.001
+Ey=0.46/NSquares
+Ez=0.26/NSquares
+
+#for i in range(0, NSquares)
+#    for j in range(0, NSquares)
+#x= 0.6
+#y= -Ey*(1+2i) #i=columna
+#z= 0.26+(Ez*(1+2*j))
+
+
 the_doc = KINBODY(
-    BODY(
-        GEOM(
-            EXTENTS("0.001 0.115 0.065"),
-            TRANSLATION("0.6 "+   "-0.8 0.32"),
-            DIFUSSECOLOR(".5 .5 .5"),
-            type="box",
-        ),
-        name="square"+str(i), type="static"
-    ),
-     name="wall",
-)  
+            BODY(
+                GEOM(
+                    EXTENTS("0.001 0.115 0.065"),
+                    TRANSLATION("0.6 "+   "-0.8 0.32"),
+                    DIFUSSECOLOR(".5 .5 .5"),
+                    type="box",
+                ),
+                name="square"+str(i), type="static"
+            ),
+            name="wall",
+        )
 
 print lxml.etree.tostring(the_doc, pretty_print=True)
