2d9988fc953c9ffc3a0305642efdae8d865e56af
Raulfdzbis <raulfernandezbis@gmail.com>
2017-12-19 19:05:52 +0100

    Parametric extents definition. Better

--- programs/wall-creator/wallCreator.py
+++ programs/wall-creator/wallCreator.py
@@ -47,7 +47,7 @@ for i in range(NSquares): #filas
         #Create pixel
         Body = etree.SubElement(KinBody, "Body", name="square"+str(Number), type="static")
         Geom = etree.SubElement(Body, "Geom", type="box")
-        Extents = etree.SubElement(Geom, "Extents").text= "0.001 "+ str(Ey)+" "+str(Ez)
+        Extents = etree.SubElement(Geom, "Extents").text= str(Ey)+" "+ str(Ey)+" "+str(Ez)
         Translation = etree.SubElement(Geom, "Translation").text= str(x)+" "+str(y)+" "+str(z)
         DifusseColor = etree.SubElement(Geom, "diffuseColor").text= ".5 .5 .5"
 
