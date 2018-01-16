04e32a99ec6e2aafae082475e733f19af271ab05
Raulfdzbis <raulfernandezbis@gmail.com>
2018-01-16 12:27:31 +0100

    some changes to make OET work with 64 sq

--- programs/cgdaExecutionOET/cgdaExecutionOETOpenrave/runIronOET.py
+++ programs/cgdaExecutionOET/cgdaExecutionOETOpenrave/runIronOET.py
@@ -35,51 +35,49 @@ import time
 # 2) Implement in CgdaExecutionOET that the outputs of paintsquared are send to the txt called before.
 # 3) Implement a while that is running until certain condition is achieved. Maybe extract this condition from evMono_ecf_params, just for simplicity.
 Gen=0
-#fout= open('percentageOET.txt', 'a')
+fout= open('percentageOET.txt', 'a')
 
-#fout2= open('DataOET++.txt', 'a')
-#fout2.write("\n")
-#fout2.write("I: ")
-#fout2.close()
+fout2= open('DataOET++.txt', 'a')
+fout2.write("\n")
+fout2.write("I: ")
+fout2.close()
+
+Nsquares=64
 
 while 1:
     f = open('memoryOET.txt', 'r')
     data_array=[] #Array of strings
+    suma = 0
 
     #Read all the numbers in memoryOET.txt
     for line in f:
         for x in line.split():
             data_array.append(x)
+            if x == "1":
+                suma += 1
+        print("sum:",suma)
     if not data_array:
-        data_array=[ "0","0","0","0"]
 	print"Empty Memory"
-
-    print(data_array[0])
-
-    #sum=0
-    #for i in data_array:
-    #    sum=sum+int(i)
+        for x in range(Nsquares):
+            data_array.append("0")
+            
+    sum=0
+    for i in data_array:
+        sum=sum+int(i)
     
     #Output percentage
-    #print sum
-    #percentage=(sum/16.0)*100
-    #percentage=str(percentage)
-    #print percentage
-    #fout.write(percentage)
-    #fout.write(" ")
-    fout2= open('DataOET++.txt', 'a')
-    #fout2.write("\n")
-    #fout2.write("I: ")
-    for i in range(len(data_array)):
-        fout2.write(data_array[i])
-        fout2.write(" ")
-
-    #,data_array[1], data_array[2],data_array[3],data_array[4],data_array[5])
-    if data_array[0]=='8':
+    print sum
+    percentage=(sum/float(Nsquares))*100.0
+    percentage=str(percentage)
+    print percentage
+    fout.write(percentage)
+    fout.write(" ")
+    if sum==Nsquares or Gen==30:
+	fout2= open('DataOET++.txt', 'a')
+	fout2.write("\n")
+	fout2.write("I: ")
         fout2.close()
         break
-    
-    print data_array
 
     #Now delete the content in the file
     f.close()
@@ -89,24 +87,27 @@ while 1:
 
     start_time = time.time()
 
-    #args = ("mpiexec", "-n", "1", "./cgdaExecutionOETOpenrave", "../../programs/cgdaExecutionOET/conf/evMono_ecf_params.xml",data_array[0],data_array[1], data_array[2],data_array[3],data_array[4],data_array[5])
-    args = ("./cgdaExecutionOETOpenrave", "../../programs/cgdaExecutionOET/conf/evMono_ecf_params.xml",data_array[0],data_array[1], data_array[2],data_array[3])
-    
-    popen = subprocess.Popen(args, stdout=subprocess.PIPE) #Pipe is the standard stdout
-    output = popen.stdout.read()
-    print output #Prints all the results of the popen proces
+    #args = ("mpiexec", "-n", "1", "./cgdaExecutionOETOpenrave", "../../programs/cgdaExecutionOET/conf/evMono_ecf_params.xml")
+
+    print data_array
+
+    popen = subprocess.Popen(["mpiexec", "-n", "1", "./cgdaExecutionOETOpenrave", "../../programs/cgdaExecutionOET/conf/evMono_ecf_params.xml"] + data_array, stdout=subprocess.PIPE) #Pipe is the standard stdout
+    #popen.wait()
+    #output = popen.stdout.read()
+    #print output
     out, err = popen.communicate()
-    #errcode = popen.returncode
+    errcode = popen.returncode
     #output = popen.stdout.read()
     ###print out
 
     #Output time
     elapsed_time=time.time() - start_time
     elapsed_time=str(elapsed_time)
-    fout2.write(elapsed_time)
-    fout2.write("\n")
+    fout.write(elapsed_time)
+    fout.write("\n")
 
     print"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
     Gen=Gen+1;
 
 print "Done!!!!!"
+
